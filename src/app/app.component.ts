2422779a184e57d3d152e1fd4f82c2212d587664
k.santhosh <santh2000916@gmail.com>
2021-06-19 16:45:58 +0530

    portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit} from '@angular/core';
-declare const $: any;
-declare var main : any;
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'portfolio';
-
-  ngOnInit(): void {
-    main();
-  }
 }
